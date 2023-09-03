d2f6b6c584151c037559449eec60dfbc3812537c
Diego581 <diego445.gonzalez@gmail.com>
2023-09-03 00:55:32 -0300

    a

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,7 +1,6 @@
-import { getLocaleDateFormat } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Categorias } from './categorias';
+
 
 @Injectable({
   providedIn: 'root',
@@ -13,10 +12,10 @@ export class InMemoryDataService implements InMemoryDbService {
       {id: 2,  nombre: 'prestamos', descripcion: 'Ingreso de dinero por un prestamo'},
       {id: 3,  nombre: 'facturas', descripcion: 'Pago de facturas'},
       ];
-    const movimiento = [
+    const movimientos = [
       {id: 1,  nombre: 'Ingreso', detalle: 'Cobro de sueldo', precio:'$300000', categoria:'1', fecha : ''},
       {id: 2,  nombre: 'Egreso', detalle: 'Pago de cuotas', precio:'$10', categoria:'3', fecha : ''},
       {id: 3,  nombre: 'Ingreso', detalle: 'Venta', precio:'$20', categoria:'2', fecha : ''},
     ] 
-    return {categorias, movimiento};
+    return {categorias, movimientos};
   }}
